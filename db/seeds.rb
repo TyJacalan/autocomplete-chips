53ae5ff4627f50caa44c30e99a60ed5a1d1c5c0f
Ty Jacalan <muzuuu@protonmail.com>
2024-08-21 14:22:06 +0800

    chore: append to the animals dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,15 @@ owners = Owner.all
 # Create Animals with association to Owners
 animals = [
   { name: 'Bella', age: 3, adopted: true, owner_id: owners.sample.id },
-  { name: 'Max', age: 2, adopted: false, owner_id: owners.sample.id },
+  { name: 'Becky', age: 2, adopted: false, owner_id: nil },
+  { name: 'Beckham', age: 4, adopted: false, owner_id: nil },
+  { name: 'Max', age: 2, adopted: true, owner_id: owners.sample.id },
+  { name: 'Mavis', age: 10, adopted: false, owner_id: nil },
   { name: 'Charlie', age: 1, adopted: false, owner_id: owners.sample.id },
   { name: 'Lucy', age: 5, adopted: true, owner_id: owners.sample.id },
-  { name: 'Rocky', age: 1, adopted: true, owner_id: owners.sample.id }
+  { name: 'Ludus', age: 1, adopted: false, owner_id: nil },
+  { name: 'Rocky', age: 1, adopted: true, owner_id: owners.sample.id },
+  { name: 'Rolly', age: 7, adopted: false, owner_id: nil }
 ]
 
 Animal.insert_all(animals)
